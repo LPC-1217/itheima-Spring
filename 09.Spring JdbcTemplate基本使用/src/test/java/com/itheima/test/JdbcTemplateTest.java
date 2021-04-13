ccb885d0726ea01617d151557a1dd6fda6059105
11137 <1113759581@qq.com>
2021-04-13 16:24:13 +0800

    LPC

--- 09.Spring JdbcTemplate基本使用/src/test/java/com/itheima/test/JdbcTemplateTest.java
+++ 09.Spring JdbcTemplate基本使用/src/test/java/com/itheima/test/JdbcTemplateTest.java
@@ -1,8 +1,14 @@
 package com.itheima.test;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 
 public class JdbcTemplateTest {
@@ -13,17 +19,29 @@ public class JdbcTemplateTest {
     @Test
     public void test1() throws Exception {
         //创建数据源对象
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass("com.mysql.jdbc.Driver");
-        dataSource.setDriverClass("jdbc:mysql://localhost:3306/test");
-        dataSource.setUser("root");
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUsername("root");
         dataSource.setPassword("123456");
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         //设置数据源对象
         jdbcTemplate.setDataSource(dataSource);
         //执行操作
-        int row = jdbcTemplate.update("insert into account values(?,?)", "tom", 5000);
-        System.out.println(row);
+        int Tom = jdbcTemplate.update("insert into account values(?,?)", "Tom", 2000);
+        System.out.println(Tom);
     }
+
+    /**
+     * 测试Spring产生JdbcTemplate模板对象
+     */
+    @Test
+    public void test2(){
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = app.getBean(JdbcTemplate.class);
+        int Bob = jdbcTemplate.update("insert into account values(?,?)", "Bob", 2000);
+        System.out.println(Bob);
+    }
+
 }
