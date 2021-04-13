3b745ca42769a9ea3741e39afda81e92317430c1
aigo <1113759581@qq.com>
2021-04-14 00:58:12 +0800

    LPC

--- 09.Spring JdbcTemplate基本使用/src/test/java/com/itheima/test/JdbcTemplateCRUDTest.java
+++ 09.Spring JdbcTemplate基本使用/src/test/java/com/itheima/test/JdbcTemplateCRUDTest.java
@@ -1,11 +1,16 @@
 package com.itheima.test;
 
+import com.itheima.dimain.Account;
+import javafx.scene.control.Toggle;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
@@ -22,5 +27,32 @@ public class JdbcTemplateCRUDTest {
     @Test
     public void testDelete(){
         jdbcTemplate.update("delete from account where name=?","tom");
+    }
+
+    /**
+     * 查询多个对象
+     */
+    @Test
+    public void testQueryAll(){
+        List<Account> accountList = jdbcTemplate.query("select * from account", new BeanPropertyRowMapper<Account>(Account.class));
+        System.out.println(accountList);
+    }
+
+    /**
+     * 查询一个对象
+     */
+    @Test
+    public void testQueryOne(){
+        Account tom = jdbcTemplate.queryForObject("select * from account where name=?", new BeanPropertyRowMapper<Account>(Account.class), "Tom");
+        System.out.println(tom);
+    }
+
+    /**
+     * 查询总条数
+     */
+    @Test
+    public void testQueryCount(){
+        Long count = jdbcTemplate.queryForObject("select count(*) from account", Long.class);
+        System.out.println(count);
     }
 }
